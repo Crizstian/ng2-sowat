a3ca28afcba168693b528399c905025ff02f1ffa
Cristian <cristiano.rosetti@gmail.com>
2016-03-29 17:38:40 -0600

    adding nosotros

--- app/components/landing/nosotros/nosotros.compnt.ts
+++ app/components/landing/nosotros/nosotros.compnt.ts
@@ -1,9 +1,17 @@
-import {Component} from 'angular2/core';
+import {Component,ElementRef} from 'angular2/core';
+import {HeaderCompnt} from '../header/header.compnt';
+
+declare var jQuery:any;
 
 @Component({
   selector   : 'about',
+  directives: [HeaderCompnt],
   templateUrl: 'app/components/landing/nosotros/templates/nosotros.html'
 })
 export class NosotrosCompnt{
+  constructor(private elementRef: ElementRef){}
 
+  ngOnInit(){
+    jQuery(this.elementRef.nativeElement).foundation();
+  }
 }
